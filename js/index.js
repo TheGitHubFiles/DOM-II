f175bbacb65872c3a0e2a7e1124913eb91f58996
Andrew Poppenberg <andrewpoppenberg@Andrews-MacBook-Pro.local>
2020-09-29 14:52:20 -0500

    initial commit im doing great

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 // Your code goes here
+const logo = document.querySelector(".logo-heading");
+
+logo.addEventListener("mouseover", (event) => {
+  logo.style.color = "green";
+});
+logo.addEventListener("mouseleave", (event) => {
+  logo.style.color = "black";
+});
+
+const footer = document.querySelector(".footer");
+
+window.addEventListener("load", (event) => {
+  footer.style.background = "purple";
+});
+
+const nav = document.querySelector(".nav");
+nav.addEventListener("focusin", (event) => {
+  nav.style.background = "yellow";
+});
+nav.addEventListener("focusout", (event) => {
+  nav.style.background = "green";
+});
+
+const begone = document.querySelector(".intro");
+
+begone.addEventListener("dblclick", (event) => {
+  begone.removeChild(begone.childNodes[0]);
+});
